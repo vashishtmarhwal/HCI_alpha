c961e40c1c9afc888288d0b93adddd641413935d
vashishtmarhwal <vmarhwal97@gmail.com>
2018-03-30 10:35:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/hci_alpha/kccHelpLine.java
@@ -0,0 +1,13 @@
+package com.example.android.hci_alpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kccHelpline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kcc_helpline);
+    }
+}
